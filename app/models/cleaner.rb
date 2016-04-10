a2c408b3ab991c03cfc52c8212f72bbe81a414a3
Jared Ning <jared@redningja.com>
2016-04-10 11:42:38 -0500

    Add validations and collection_select to booking.

--- app/models/cleaner.rb
+++ app/models/cleaner.rb
@@ -1,2 +1,9 @@
 class Cleaner < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :quality_score, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
+
+  def full_name
+    "#{last_name}, #{first_name}"
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,9 @@
 class Customer < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true
+
+  def full_name
+    "#{last_name}, #{first_name}"
+  end
 end
